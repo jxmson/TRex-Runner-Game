18d6defda1c771c5648b18a6f6d316deb7b45965
Nthabeleng jimson <56711591+jxmson@users.noreply.github.com>
2021-09-01 11:26:37 +0200

    Add files via upload

---
+++ TRexRunnerGame/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TRexRunnerGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pctTRex = new System.Windows.Forms.PictureBox();
+            this.tmrTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTRex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 400);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(645, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TRexRunnerGame.Properties.Resources.obstacle_1;
+            this.pictureBox2.Location = new System.Drawing.Point(373, 356);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TRexRunnerGame.Properties.Resources.obstacle_2;
+            this.pictureBox3.Location = new System.Drawing.Point(514, 369);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // pctTRex
+            // 
+            this.pctTRex.Image = global::TRexRunnerGame.Properties.Resources.running;
+            this.pctTRex.Location = new System.Drawing.Point(42, 357);
+            this.pctTRex.Name = "pctTRex";
+            this.pctTRex.Size = new System.Drawing.Size(40, 43);
+            this.pctTRex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctTRex.TabIndex = 4;
+            this.pctTRex.TabStop = false;
+            // 
+            // tmrTimer
+            // 
+            this.tmrTimer.Interval = 20;
+            this.tmrTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 22);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(106, 24);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(492, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "SPACE to jump";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(644, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pctTRex);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Tag = "obstacle";
+            this.Text = "T Rex Runner";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTRex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pctTRex;
+        private System.Windows.Forms.Timer tmrTimer;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TRexRunnerGame/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRexRunnerGame
+{
+    public partial class Form1 : Form
+    {
+        bool jump = false;
+        int jumpSpeed;
+        int force = 12; //Max height hat player can jump
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random r = new Random();
+        int position;
+        bool gameOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            pctTRex.Top += jumpSpeed;
+            lblScore.Text = "Score: " + score;
+
+            if (jump == true && force < 0)
+                jump = false;
+
+            if (jump == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+                jumpSpeed = 12;
+
+            if (pctTRex.Top > 356 && jump == false)
+            {
+                force = 12;
+                pctTRex.Top = 356;
+                jumpSpeed = 0;
+            }
+            foreach(Control c in this.Controls)
+            {
+                if (c is PictureBox && (string)c.Tag == "obstacle")
+                {
+                    c.Left -= obstacleSpeed;
+                    if (c.Left < -100)
+                    {
+                        c.Left = this.ClientSize.Width + r.Next(200, 500) + (c.Width * 15);
+                        score++;
+                    }
+                    //Collision control
+                    if(pctTRex.Bounds.IntersectsWith(c.Bounds))
+                    {
+                        tmrTimer.Stop();
+                        pctTRex.Image = Properties.Resources.dead;
+                        lblScore.Text += " Press R to restart the game.";
+                        gameOver = true;
+                    }
+                }
+            }
+
+            if (score > 10)
+                obstacleSpeed = 15; 
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jump == false)
+                jump = true;
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if(jump == true)
+                jump = false;
+
+            if (e.KeyCode == Keys.R && gameOver == true)
+                Reset();
+        }
+        private void Reset()
+        {
+            pctTRex.Image = Properties.Resources.running;
+            force = 12;
+            jumpSpeed = 0;
+            jump = false;
+            score = 0;
+            lblScore.Text = "Score: " +  score;
+            gameOver = false;
+            pctTRex.Top = 357;        
+
+            foreach(Control c in this.Controls)
+            {
+                if(c is PictureBox && (string)c.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width + r.Next(500, 800) + (c.Width * 10);
+                    c.Left = position;
+                }
+            }
+            tmrTimer.Start();
+        }
+        
+
+    }
+}
